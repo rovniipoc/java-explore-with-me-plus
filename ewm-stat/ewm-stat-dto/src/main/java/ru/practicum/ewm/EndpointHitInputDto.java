7bd7640edbd83dbc3be00ab17038fb5f95f92ec6
3agent999 <agentsmith999@yandex.ru>
2025-01-14 21:41:24 +0300

    Реализовать ДТО

---
+++ ewm-stat/ewm-stat-dto/src/main/java/ru/practicum/ewm/EndpointHitInputDto.java
@@ -0,0 +1,24 @@
+package ru.practicum.ewm;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+public class EndpointHitInputDto {
+    @NotBlank
+    private String app; // Название приложения
+
+    @NotBlank
+    private String uri; // URI запроса
+
+    @NotBlank
+    private String ip; // IP пользователя
+
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    private LocalDateTime timestamp; // Временная метка запроса
+}
