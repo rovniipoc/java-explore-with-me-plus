5365d087cad35b45a277dbbf1ed7c31ffd0c316b
Kirill Malskiy <sabbat@mail.ru>
2025-01-27 19:48:46 +0300

    удалил неиспользуемые импорты

--- ewm-stat/ewm-stat-client/src/main/java/ru/practicum/ewm/exeption/ErrorHandler.java
+++ ewm-stat/ewm-stat-client/src/main/java/ru/practicum/ewm/exeption/ErrorHandler.java
@@ -1,16 +1,12 @@
 package ru.practicum.ewm.exeption;
 
-import jakarta.validation.ConstraintViolationException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 @RestControllerAdvice
 @Slf4j
